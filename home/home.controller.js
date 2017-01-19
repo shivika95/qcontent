01bba91d1275271e923a1dbe77d7face12f685c5
shipra <rawatshipra15@gmail.com>
2017-01-19 20:25:58 +0530

    no message

--- home/home.controller.js
+++ home/home.controller.js
@@ -35,6 +35,16 @@ var app = angular.module('app')
 		},
 		show:false
 	    }
+        $scope.clock = "loading clock..."; // initialise the time variable
+                $scope.tickInterval = 1000 //ms
+
+                var tick = function() {
+                    $scope.clock = Date.now() // get the current time
+                    $timeout(tick, $scope.tickInterval); // reset the timer
+                }
+
+                // Start the timer
+                $timeout(tick, $scope.tickInterval);
             
 	    $scope.getYouTubePlayerInstance=function(){
 		var player=new YT.Player( "youtube_advertisement_player",{
