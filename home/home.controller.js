1626eaefc3bbfd65d071d8024956e15836b73179
venkatraopasupuleti <venkatraopasupuleti93@gmail.com>
2017-01-23 21:05:24 +0530

    trying to fix the audio issue

--- home/home.controller.js
+++ home/home.controller.js
@@ -428,10 +428,10 @@ var app = angular.module('app')
                 for (var i = 0; i < $scope.advertisements.length; i++) {
                     $scope.advertisements[i].show = false;
                 }
-        $scope.youtube_advertisement_player.show=false
-        if($scope.youtube_advertisement_player.player){
-            $scope.youtube_advertisement_player.player.pauseVideo()
-        }
+        	if($scope.youtube_advertisement_player.player){
+            		$scope.youtube_advertisement_player.player.pauseVideo()
+        	}
+		$scope.youtube_advertisement_player.show=false
             }
 
             function showDoc() {
